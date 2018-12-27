f29beb7c3d3169bbf04d4e8067023ac6cc714021
Maxim Stykow <46091730+mstykow@users.noreply.github.com>
2018-12-27 14:44:04 -0800

    Add files via upload

---
+++ commacode.py
@@ -0,0 +1,18 @@
+def enum(list):
+    a = ''
+    for i in list[:-1]:
+        a = a + i + ', '
+    a = a + 'and ' + list[-1]
+    return a
+
+sampleList = []
+while True:
+    print('Enter list item ' + str(len(sampleList) + 1) +
+      ' (Or enter nothing to stop.):')
+    item = input()
+    if item == '':
+        break
+    # sampleList = sampleList + [item] # Option to concatenate
+    sampleList.append(item) # Option to append
+
+print(enum(sampleList))
